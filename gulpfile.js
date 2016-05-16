c6340865fe2b03a1edfd3a01d67bd68e793239d1
Mark Brown II <mbrownii@doc.gov>
2016-05-16 10:50:44 -0400

    Make indentation from tab to 4-spaces. Add line for external sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -13,65 +13,65 @@ var autoprefixer = require('gulp-autoprefixer'); // adds prefixes to CSS files
 var jshint = require('gulp-jshint'); // lints JS files
 
 gulp.task('sass', function() {
-	return gulp.src('app/scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(sourcemaps.write())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+    }))
 })
 
 gulp.task('browserSync', function() {
-	browserSync.init({
-		server: {
-			baseDir: "app"
-		}
-	});
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
 });
 
 gulp.task('useref', function() {
-	return gulp.src('app/*.html')
-		.pipe(useref())
-		.pipe(gulpIf('*.js', jshint()))
-		.pipe(gulpIf('*.js', jshint.reporter('default')))
-		.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulpIf('*.css', cssnano()))
-		.pipe(gulp.dest('dist'))
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', jshint()))
+        .pipe(gulpIf('*.js', jshint.reporter('default')))
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
 })
 
 gulp.task('clean:dist', function() {
-	return del.sync('dist');
+    return del.sync('dist');
 })
 
 gulp.task('jsscripts', function() {
-	gulp.src(['app/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(sourcemaps.init())
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(sourcemaps.write('../maps'))
-		.pipe(gulp.dest('app/'))
-		.pipe(browserSync.reload({stream:true}))
+    gulp.src(['app/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(sourcemaps.init())
+        .pipe(babel({presets: ['es2015']}))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('app/'))
+        .pipe(browserSync.reload({stream:true}))
 })
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
-	gulp.watch('app/scss/**/*.scss', ['sass']);
-	gulp.watch('app/*.html', browserSync.reload);
-	gulp.watch('app/*.js', browserSync.reload);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/*.js', browserSync.reload);
 })
 
 gulp.task('build', function(callback) {
-	runSequence('clean:dist', 
-		['sass', 'useref'],
-		callback
-		)
+    runSequence('clean:dist', 
+        ['sass', 'useref'],
+        callback
+        )
 })
 
 gulp.task('default', function(callback) {
-	runSequence(['sass', 'browserSync', 'watch', 'jsscripts'],
-		callback
-		)
+    runSequence(['sass', 'browserSync', 'watch', 'jsscripts'],
+        callback
+        )
 })
