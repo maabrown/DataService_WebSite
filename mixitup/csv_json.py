f6927e5d6c47d6c016e82474a84ed923a96dc805
Mark Brown II <mbrownii@doc.gov>
2016-05-20 10:22:41 -0400

    Add Hanldebars to handle creation of thumbnail and modal views
    Make sure to keep the one div with an id=Container for MixItUp and use
    .container in CSS to style things.

--- mixitup/csv_json.py
+++ mixitup/csv_json.py
@@ -2,13 +2,13 @@ import csv, ujson as json, pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-def mapjson(course, uid, course_key = ['first_name','last_name','position','quote','quote_author','life_Story','why_cds']):
+def mapjson(course, uid, course_key = ['first_name','last_name','position','teams', 'picture_file','quote','quote_author','life_Story','why_cds']):
     course_entry = { key: value for (key, value) in zip(course_key, course) }
     course_entry['uid'] = uid
     return course_entry
 
 course_json = []
-with open('./CDS Team Responses.csv', 'rt') as f:
+with open('./CDS Team Responses - Sheet1.csv', 'rt') as f:
     courses = csv.reader(f)
     for i, course in enumerate(courses):
         # if i == 0:
